dfcd5738ceeea0b838bfdbbaeeaf9eb12ac58083
vladsoltys <soltisvlad97@gmail.com>
2022-04-28 15:44:32 +0300

    JS homework modified

--- JS_Homework_1.js
+++ JS_Homework_1.js
@@ -58,18 +58,15 @@ let age_2 = 18;
 let age_3 = 60;
 //  29. Создать if в котором будите проверять значение переменной age_1
 //  30. Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is ” + age_1 + “ It’s less then ”
+//  31. Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome  !”
+//  32. Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”.
+//  33. Иначе выводите “Technical work”.
 if (age_1 < age_2) {
     console.log("You don't have access cause your age is " + age_1 + " It's less then 18");
-} 
-//  31. Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome  !”
-else if (age_1 >= age_2 && age_1 < age_3) {
+} else if (age_1 >= age_2 && age_1 < age_3) {
     console.log("Welcome  !");
-} 
-//  32. Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”.
-else if (age_1 > age_3) {
+} else if (age_1 > age_3) {
     console.log("Keep calm and look Culture channel");
-} 
-//  33. Иначе выводите “Technical work”.
-else {
+} else {
     console.log("Technical work");
 };

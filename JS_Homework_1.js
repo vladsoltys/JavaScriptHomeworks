55496e4a5c957cbffc8a3a323d2802ca92b2bf82
vladsoltys <soltisvlad97@gmail.com>
2022-04-28 15:30:54 +0300

    JS modified

--- JS_Homework_1.js
+++ JS_Homework_1.js
@@ -39,14 +39,14 @@ item_6 = 15;
 //  20. Присвоить переменной item_6_type тип переменной item_6
 item_6_type = typeof(item_6);
 //  21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——
-console.log("item_6 == ",  item_6);
+console.log("item_6 == ",  typeof(item_6));
 console.log("item_6_type == ",  item_6_type);
 //  22. Создать переменную item_7 и в ней преобразовать item_6 в String.
 let item_7 = String(item_6);
 //  23. Создать переменную item_7_type
 let item_7_type
 //  24. Присвоить переменной item_7_type тип переменной item_7
-item_7_type = item_7;
+item_7_type = typeof(item_7);
 //  25. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——  
 console.log("item_7 == ",  typeof(item_7));
 console.log("item_7_type == ",  item_7_type);
